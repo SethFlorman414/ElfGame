daab7ff18024bc92cf268c41beeb09b5251b336c
Seth Florman <18.seth.florman@ksd.org>
2018-04-23 13:53:30 -0700

    Level Design

---
+++ TrialsofIlyana/Assets/OpenDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour {
+
+    // Use this for initialization
+
+    public Animator door;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collision collision)
+    {
+
+        if(collision.gameObject.tag == "Player")
+        {
+
+            door.SetBool("Open", true);
+
+        }
+
+
+    }
+
+}
