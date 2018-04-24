636937167e8dcdc7f844f0cd2be4caabebc0f965
SethFlorman414 <plankda2x4@gmail.com>
2018-04-23 17:03:36 -0700

    Foo

---
+++ TrialsofIlyana/Assets/AI_Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+
+public class AI_Controller : MonoBehaviour {
+
+    // Use this for initialization
+
+
+    [SerializeField] private NavMeshAgent m_NavMashAgent;
+    [SerializeField] private GameObject m_Target;
+
+
+    void Start () {
+
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        m_NavMashAgent.SetDestination(m_Target.transform.position);
+
+	}
+}
