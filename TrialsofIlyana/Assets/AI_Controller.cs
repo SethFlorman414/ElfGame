44072ddb436ea3fc5f89c7af82ed494dc719250a
SethFlorman414 <plankda2x4@gmail.com>
2018-04-24 09:39:35 -0700

    Tried to do AI. Still working on it. SF

--- TrialsofIlyana/Assets/AI_Controller.cs
+++ TrialsofIlyana/Assets/AI_Controller.cs
@@ -9,23 +9,69 @@ public class AI_Controller : MonoBehaviour {
 
     // Use this for initialization
 
-
+    [Header("NavMesh Settings / Target")]
     [SerializeField] private NavMeshAgent m_NavMashAgent;
     [SerializeField] private GameObject m_Target;
 
+    [Header("Animation Settings")]
+    [SerializeField] private Animator m_Animator;
+    [SerializeField] private CurrentState m_CurrentState;
 
     void Start () {
-
-
-
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 
-        m_NavMashAgent.SetDestination(m_Target.transform.position);
+        //m_NavMashAgent.SetDestination(m_Target.transform.position);
+        AnimationChecker();
 
 	}
+
+    private void AnimationChecker()
+    {
+        if(m_CurrentState == CurrentState.Idle)
+        {
+
+            m_Animator.SetBool("isWalking", false);
+            m_Animator.SetBool("isRunning", false);
+
+        }
+        else if (m_CurrentState == CurrentState.Walking)
+        {
+
+            m_Animator.SetBool("isWalking", true);
+            m_Animator.SetBool("isRunning", false);
+
+        }
+        else if (m_CurrentState == CurrentState.Running)
+        {
+
+            m_Animator.SetBool("isWalking", false);
+            m_Animator.SetBool("isRunning", true);
+
+        }
+
+        
+
+    }
+
+    IEnumerator TestSwitch()
+    {
+
+        yield return new WaitForSeconds(2);
+        m_CurrentState = CurrentState.Running;
+
+    }
+
+    enum CurrentState
+    {
+
+        Idle,
+        Walking,
+        Running
+
+    }
 }
